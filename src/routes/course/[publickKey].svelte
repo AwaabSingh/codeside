d523a4f6e0c64f134e865d5dad3124471f4f1a0d
Abdulkereem <abdulkereemokereem@gmail.com>
2022-03-26 16:02:55 +0100

    check it out

--- src/routes/course/[publickKey].svelte
+++ src/routes/course/[publickKey].svelte
@@ -1,5 +1,13 @@
 <script>
       import { page } from '$app/stores';
+      import { onMount } from 'svelte';
         console.log($page.params.publickKey)
+
+        let course 
+
+        onMount(async ()=>{
+          course = await fetch(`https://aqueous-beyond-13704.herokuapp.com/getcousebyid?coursePk=${url.publickKey}`)
+
+        })
 </script>
 <h1>Halo</h1>
