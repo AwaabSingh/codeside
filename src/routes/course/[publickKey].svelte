7f68c6f07580e955b3ed5e9f4979587a03c36870
Abdulkereem <abdulkereemokereem@gmail.com>
2022-03-26 16:06:37 +0100

    again Legend

--- src/routes/course/[publickKey].svelte
+++ src/routes/course/[publickKey].svelte
@@ -2,11 +2,12 @@
       import { page } from '$app/stores';
       import { onMount } from 'svelte';
         console.log($page.params.publickKey)
-
+        const pk = $page.params.publickKey
         let course 
 
         onMount(async ()=>{
-          course = await fetch(`https://aqueous-beyond-13704.herokuapp.com/getcousebyid?coursePk=${url.publickKey}`)
+          course = await fetch(`https://aqueous-beyond-13704.herokuapp.com/getcousebyid?coursePk=${pk}`)
+          console.log(course);
 
         })
 </script>
